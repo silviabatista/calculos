8a4e8b454db8268f39fb0a405184adc0e8470f11
Silvia Batista <156548516+silviabatista@users.noreply.github.com>
2024-02-03 18:59:59 +0000

    Acrescentar código para testar

--- Program.cs
+++ Program.cs
@@ -13,6 +13,12 @@ namespace Calculos
             Console.WriteLine("Aplicação Cálculos");
             Console.WriteLine($"4 + 2 = {Aritmetica.Somar(4, 2)}");
             Console.WriteLine($"4 - 2 = {Aritmetica.Subtrair(4, 2)}");
+
+            double t1 = Conversoes.ConversaoTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 36);
+            double t2 = Conversoes.ConversaoTemperatura(Conversoes.ConversaoTemperatura.FahrenhitCelsius, 100);
+
+            Console.WriteLine($"36º Celsius = {t1}º Fahrenheit");
+            Console.WriteLine($"100º Fahrenheit = {t2}º Celsius")
             Console.ReadKey();
         }
     }
